de842546cc6dd0d02cf77b85fa4b63402d4eab15
mask-pp <huan_zi266@sina.com>
2018-12-24 14:32:33 +0800

    modify the path of used package.

--- metrics/influxdb/influxdb.go
+++ metrics/influxdb/influxdb.go
@@ -5,8 +5,8 @@ import (
 	uurl "net/url"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/QuarkChain/goquarkchain/log"
+	"github.com/QuarkChain/goquarkchain/metrics"
 	"github.com/influxdata/influxdb/client"
 )
 
