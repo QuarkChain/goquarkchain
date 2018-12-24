bc65917c8f5c58626f24a16d3e6045fe998fe2f6
ninjaahhh <38544039+ninjaahhh@users.noreply.github.com>
2018-12-24 12:00:32 -0800

    [consensus p1] Implement (simplified) qkchash (#9)

---
+++ consensus/qkchash/algorithm.go
@@ -0,0 +1,120 @@
+package qkchash
+
+import (
+	"encoding/binary"
+	"sort"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+const (
+	accessRound   = 64
+	cacheEntryCnt = 1024 * 64
+)
+
+type qkcCache struct {
+	ls  []uint64
+	set map[uint64]struct{}
+}
+
+// fnv64 is an algorithm inspired by the FNV hash, which in some cases is used as
+// a non-associative substitute for XOR. This is 64-bit version.
+func fnv64(a, b uint64) uint64 {
+	return a*0x100000001b3 ^ b
+}
+
+// generateQKCCache is a simpler implementation for generating cache for qkchash.
+// Can be improved using a balanced tree.
+func generateQKCCache(cnt int, seed []byte) qkcCache {
+	ls := []uint64{}
+	set := make(map[uint64]struct{})
+	for i := uint32(0); i < uint32(cnt/8); i++ {
+		iBytes := make([]byte, 4)
+		binary.BigEndian.PutUint32(iBytes, i)
+		bs := crypto.Keccak512(append(seed, iBytes...))
+		// Read 8 bytes as uint64
+		for j := 0; j < len(bs); j += 8 {
+			ele := binary.LittleEndian.Uint64(bs[j:])
+			if _, ok := set[ele]; !ok {
+				ls = append(ls, ele)
+				set[ele] = struct{}{}
+			}
+		}
+	}
+	sort.Slice(ls, func(i, j int) bool { return ls[i] < ls[j] })
+	return qkcCache{ls, set}
+}
+
+// qkcHash runs the order-statistics-based hash algorithm.
+func qkcHash(hash, nonceBytes []byte, cache qkcCache) (digest []byte, result []byte) {
+	const mixBytes = 128
+	// Copy the cache since modification is needed
+	cacheLs := make([]uint64, len(cache.ls))
+	copy(cacheLs, cache.ls)
+	cacheSet := make(map[uint64]struct{})
+	for k, v := range cache.set {
+		cacheSet[k] = v
+	}
+	// Combine header+nonce into a seed
+	seed := append(hash, nonceBytes...)
+	seed = crypto.Keccak512(seed)
+	seedHead := binary.LittleEndian.Uint64(seed)
+
+	// Start the mix with replicated seed
+	mix := make([]uint64, mixBytes/8)
+	for i := 0; i < len(mix); i++ {
+		mix[i] = binary.LittleEndian.Uint64(seed[i%8*8:])
+	}
+
+	// TODO: can be improved using balanced tree
+	for i := 0; i < accessRound; i++ {
+		newData := make([]uint64, mixBytes/8)
+		p := fnv64(uint64(i)^seedHead, mix[i%len(mix)])
+		for j := 0; j < len(mix); j++ {
+			idx := p % uint64(len(cacheLs))
+			v := cacheLs[idx]
+			newData[j] = v
+			cacheLs = append(cacheLs[:idx], cacheLs[idx+1:]...)
+			delete(cacheSet, v)
+
+			// Generate a random item and insert
+			p = fnv64(p, v)
+			if _, ok := cacheSet[p]; !ok {
+				// Insert in a sorted order
+				sz := len(cacheLs)
+				idxLess := sort.Search(sz, func(i int) bool { return cacheLs[i] > p })
+				switch idxLess {
+				case 0:
+					cacheLs = append([]uint64{p}, cacheLs...)
+				case sz:
+					cacheLs = append(cacheLs, p)
+				default:
+					cacheLs = append(cacheLs, 0)
+					copy(cacheLs[idxLess+1:], cacheLs[idxLess:])
+					cacheLs[idxLess] = p
+				}
+				cacheSet[p] = struct{}{}
+			}
+			// Continue next search
+			p = fnv64(p, v)
+		}
+
+		for j := 0; j < len(mix); j++ {
+			mix[j] = fnv64(mix[j], newData[j])
+		}
+	}
+
+	// Compress mix
+	for i := 0; i < len(mix); i += 4 {
+		mix[i/4] = fnv64(fnv64(fnv64(mix[i], mix[i+1]), mix[i+2]), mix[i+3])
+	}
+	mix = mix[:len(mix)/4]
+
+	digest = make([]byte, common.HashLength)
+	for i, val := range mix {
+		binary.LittleEndian.PutUint64(digest[i*8:], val)
+	}
+	result = crypto.Keccak256(append(seed, digest...))
+	return digest, result
+}
---
+++ consensus/qkchash/algorithm_test.go
@@ -0,0 +1,41 @@
+package qkchash
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateQKCCache(t *testing.T) {
+	cache := generateQKCCache(cacheEntryCnt, nil)
+	assert.Equal(t, cacheEntryCnt, len(cache.ls))
+	for i := 1; i < len(cache.ls); i++ {
+		assert.True(t, cache.ls[i-1] < cache.ls[i])
+	}
+	assert.Equal(t, uint64(71869947341538), cache.ls[0])
+}
+
+func TestQKCHash(t *testing.T) {
+	assert := assert.New(t)
+	cache := generateQKCCache(cacheEntryCnt, nil)
+	digest, result := qkcHash([]byte{}, []byte{}, cache)
+	assert.Equal(
+		"be3913b35b8815a61146f4d8dcce89a2bd8a262698c7263a56abe14b8e29595c",
+		fmt.Sprintf("%x", digest),
+	)
+	assert.Equal(
+		"bee2c41fce7183e73823dbefa846439817af68d28891f58ce8331cca7a871504",
+		fmt.Sprintf("%x", result),
+	)
+
+	digest, result = qkcHash([]byte("Hello World!"), []byte{}, cache)
+	assert.Equal(
+		"53423968b44d02b1861e9a000d035d74bc0fa515e8f702261bcfcc3e5fb4bd3d",
+		fmt.Sprintf("%x", digest),
+	)
+	assert.Equal(
+		"992923af6a261ef3f0b7c82b57ab28c055e31021d0d6258f7dcfd9d6195e0c70",
+		fmt.Sprintf("%x", result),
+	)
+}
