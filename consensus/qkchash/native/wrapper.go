1ba9dbf28f6ba5897060339e08b67f4538a29b63
ninjaahhh <junjiah@protonmail.com>
2019-01-04 18:13:30 -0800

    Minor: improve type export

--- consensus/qkchash/native/wrapper.go
+++ consensus/qkchash/native/wrapper.go
@@ -2,13 +2,15 @@ package native
 
 import "errors"
 
-// Cache is the type of native qkchash impl.
-type Cache struct {
+// Cache is the type of cache in native qkchash impl.
+type Cache = *cache
+
+type cache struct {
 	ptr *uintptr
 }
 
 // Destroy calls the underlying cpp func to free the memory.
-func (c *Cache) Destroy() {
+func (c *cache) Destroy() {
 	if c.ptr == nil {
 		panic("destroy non-existent native cache")
 	}
@@ -17,13 +19,13 @@ func (c *Cache) Destroy() {
 }
 
 // NewCache creates the qkchash cache for cpp impl.
-func NewCache(rawCache []uint64) *Cache {
+func NewCache(rawCache []uint64) Cache {
 	nativeCache := Cache_create(rawCache)
-	return &Cache{&nativeCache}
+	return &cache{&nativeCache}
 }
 
 // Hash wraps the native qkchash algorithm.
-func Hash(cache *Cache, seed [8]uint64) (ret [4]uint64, err error) {
+func Hash(cache Cache, seed [8]uint64) (ret [4]uint64, err error) {
 	if cache == nil || cache.ptr == nil {
 		return ret, errors.New("invoking native qkchash on empty cache")
 	}
--- consensus/qkchash/native/wrapper_test.go
+++ consensus/qkchash/native/wrapper_test.go
@@ -17,9 +17,10 @@ func TestCacheCreateAndDestroy(t *testing.T) {
 func TestHash(t *testing.T) {
 	assert := assert.New(t)
 
+	var c Cache
 	// Failure case
-	cache := &Cache{nil}
-	_, err := Hash(cache, [8]uint64{}) // Invalid cache
+	c = &cache{nil}
+	_, err := Hash(c, [8]uint64{}) // Invalid cache
 	assert.Error(err)
 
 	// Success
@@ -28,15 +29,15 @@ func TestHash(t *testing.T) {
 	for i := 0; i < len(rawCache); i++ {
 		rawCache[i] = uint64(i * 2)
 	}
-	cache = NewCache(rawCache)
-	defer cache.Destroy()
+	c = NewCache(rawCache)
+	defer c.Destroy()
 
 	seed := [8]uint64{}
 	for i := 0; i < len(seed); i++ {
 		seed[i] = uint64(i + 1)
 	}
 
-	ret, err := Hash(cache, seed)
+	ret, err := Hash(c, seed)
 	// Verified with cpp-py version
 	expected := [4]uint64{0x720e8da04bf3b232, 0x8dbfff09fd460b1c, 0xa6355a7ce4041df8, 0x968d37a76ffa20ee}
 	assert.NoError(err)
