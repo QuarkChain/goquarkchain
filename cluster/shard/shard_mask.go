80b60a4b5a396ba258639c3e517da4e0f69595cb
mask-pp <huan_zi266@sina.com>
2019-01-12 10:37:03 +0800

    remove some not related functions.

--- cluster/shard/shard_mask.go
+++ cluster/shard/shard_mask.go
@@ -1,7 +1,5 @@
 package shard
 
-import "github.com/QuarkChain/goquarkchain/cluster"
-
 type ShardMask struct {
 	value uint64
 }
@@ -10,19 +8,6 @@ func NewShardMask(value uint64) *ShardMask {
 	return &ShardMask{
 		value: value,
 	}
-}
-
-func (s *ShardMask) ContainShardId(shardId uint64) bool {
-	bitMask := uint64((1 << (cluster.IntLeftMostBit(s.value) - 1)) - 1)
-	return (bitMask & shardId) == (s.value & bitMask)
-}
-
-// TODO Supplement the implementation of this function
-/*func (s *ShardMask) Contain_branch()
-*/
-
-func (s *ShardMask) HasOverlap(value uint64) bool {
-	return cluster.MasksHaveOverlap(s.value, value)
 }
 
 // TODO Supplement the Iterate
