a1dbbb383181e11f830e189ec9e65ea985f1615a
sunchunfeng <516108736@qq.com>
2019-03-20 20:06:28 +0800

    add comment for cross shard contract

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -701,7 +701,8 @@ func opCreate(pc *uint64, interpreter *EVMInterpreter, contract *Contract, memor
 	}
 
 	contract.UseGas(gas)
-	res, addr, returnGas, suberr := interpreter.evm.Create(contract, input, gas, value, false, 0) //TODO : "isCrossShard == false" is ok?
+	//TODO:not support crossShard contract so this must be "false" .anything else question?
+	res, addr, returnGas, suberr := interpreter.evm.Create(contract, input, gas, value, false, 0)
 	// Push item on the stack based on the returned error. If the ruleset is
 	// homestead we must check for CodeStoreOutOfGasError (homestead only
 	// rule) and treat as an error, if the ruleset is frontier we must
