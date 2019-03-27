86ef67c4644e01fe0cd3349163930f4e0648ded4
mask-pp <huanliu@quarkchain.org>
2019-03-26 20:34:52 -0700

    modify stop grpc client method

--- cluster/rpc/grpc_client.go
+++ cluster/rpc/grpc_client.go
@@ -9,7 +9,6 @@ import (
 	"google.golang.org/grpc/connectivity"
 	"reflect"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -99,7 +98,7 @@ type RPClient struct {
 	timeout time.Duration
 	conns   map[string]*grpc.ClientConn
 	funcs   map[int64]opType
-	run     int64
+	running int64
 }
 
 func NewRPCLient() *RPClient {
@@ -107,7 +106,7 @@ func NewRPCLient() *RPClient {
 		conns:   conns,
 		funcs:   rpcFuncs,
 		timeout: 10 * time.Second,
-		run:     1,
+		running: 1,
 	}
 }
 
@@ -129,7 +128,7 @@ func (c *RPClient) GetOpName(op int64) string {
 
 func (c *RPClient) GetMasterServerSideOp(target string, req *Request) (*Response, error) {
 
-	if atomic.LoadInt64(&c.run) == 1 {
+	if c.running == 1 {
 		if !c.checkOp(req.Op, MasterServer) {
 			return nil, errors.New(fmt.Sprintf("%s don't belong to master server grpc functions", rpcFuncs[req.Op].name))
 		}
@@ -145,7 +144,7 @@ func (c *RPClient) GetMasterServerSideOp(target string, req *Request) (*Response
 
 func (c *RPClient) GetSlaveSideOp(target string, req *Request) (*Response, error) {
 
-	if atomic.LoadInt64(&c.run) == 1 {
+	if c.running == 1 {
 		if !c.checkOp(req.Op, SlaveServer) {
 			return nil, errors.New(fmt.Sprintf("%s don't belong to slave server grpc functions", rpcFuncs[req.Op].name))
 		}
@@ -160,7 +159,9 @@ func (c *RPClient) GetSlaveSideOp(target string, req *Request) (*Response, error
 }
 
 func (c *RPClient) Close() {
-	atomic.StoreInt64(&c.run, 0)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.running = 0
 	for _, client := range c.conns {
 		client.Close()
 	}
@@ -213,7 +214,7 @@ func (c *RPClient) addConn(target string) error {
 }
 
 func (c *RPClient) GetConn(target string) (*grpc.ClientConn, error) {
-	if atomic.LoadInt64(&c.run) == 1 {
+	if c.running == 1 {
 		if conn, ok := conns[target]; !ok ||
 			(ok && conn.GetState() > connectivity.TransientFailure) {
 			if err := c.addConn(target); err != nil {
