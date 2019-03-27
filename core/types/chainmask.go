212b7772ceb2c0de593b4a26fc51c5b5b9ae0c6a
mask-pp <32827930+mask-pp@users.noreply.github.com>
2019-03-27 10:02:05 +0800

    add common module (#71)
    * move IsP2, IntLeftMostBit func from other modules to common module
    
    * modify import path

--- core/types/chainmask.go
+++ core/types/chainmask.go
@@ -27,7 +27,7 @@ func NewChainMask(value uint32) *ChainMask {
 
 func (c *ChainMask) ContainFullShardId(fullShardId uint32) bool {
 	chainId := fullShardId >> 16
-	bitMask := uint32((1 << (account.IntLeftMostBit(c.value) - 1)) - 1)
+	bitMask := uint32((1 << (common.IntLeftMostBit(c.value) - 1)) - 1)
 	return (bitMask & chainId) == (c.value & bitMask)
 }
 
