126290b1f94a70163bc86395f7cb690f9e008e77
mask-pp <huanliu@quarkchain.org>
2019-04-03 01:01:06 -0700

    modify http port

--- cluster/service/defaults.go
+++ cluster/service/defaults.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	DefaultHTTPHost = "localhost" // Default host interface for the HTTP RPC server
-	DefaultHTTPPort = 8545        // Default TCP port for the HTTP RPC server
+	DefaultHTTPPort = 38391       // Default TCP port for the HTTP RPC server
 	DefaultWSHost   = "localhost" // Default host interface for the websocket RPC server
 	DefaultWSPort   = 8546        // Default TCP port for the websocket RPC server
 )
