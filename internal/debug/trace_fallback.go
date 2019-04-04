0adf6edafa59ecf132d8b44950456830916b1daa
mask-pp <huanliu@quarkchain.org>
2019-04-03 22:37:47 -0700

    add qkcapi and debug module

---
+++ internal/debug/trace_fallback.go
@@ -0,0 +1,17 @@
+// Modified from go-ethereum under GNU Lesser General Public License
+
+//+build !go1.5
+
+// no-op implementation of tracing methods for Go < 1.5.
+
+package debug
+
+import "errors"
+
+func (*HandlerT) StartGoTrace(string) error {
+	return errors.New("tracing is not supported on Go < 1.5")
+}
+
+func (*HandlerT) StopGoTrace() error {
+	return errors.New("tracing is not supported on Go < 1.5")
+}
