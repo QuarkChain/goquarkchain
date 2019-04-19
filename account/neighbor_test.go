70a8729addea7dd335465de06cc2449462134067
sunchunfeng <516108736@qq.com>
2019-04-19 17:38:47 +0800

    add assert to check test

--- account/neighbor_test.go
+++ account/neighbor_test.go
@@ -1,32 +1,25 @@
 package account
 
 import (
-	"errors"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestIsNeighbor(t *testing.T) {
 	b1 := NewBranch(2<<16 | 2 | 1)
 	b2 := NewBranch(2<<16 | 2 | 0)
-	if IsNeighbor(b1, b2, 33) == false {
-		panic(errors.New("should true"))
-	}
+	assert.Equal(t, IsNeighbor(b1, b2, 33), true)
 
 	b1 = NewBranch(1<<16 | 2 | 1)
 	b2 = NewBranch(3<<16 | 2 | 1)
-	if IsNeighbor(b1, b2, 33) == false {
-		panic(errors.New("should true"))
-	}
+
+	assert.Equal(t, IsNeighbor(b1, b2, 33), true)
 
 	b1 = NewBranch(1<<16 | 2 | 0)
 	b2 = NewBranch(3<<16 | 2 | 1)
-	if IsNeighbor(b1, b2, 32) == false {
-		panic(errors.New("should true"))
-	}
+	assert.Equal(t, IsNeighbor(b1, b2, 32), true)
 
 	b1 = NewBranch(1<<16 | 2 | 0)
 	b2 = NewBranch(3<<16 | 2 | 1)
-	if IsNeighbor(b1, b2, 33) == true {
-		panic(errors.New("should false"))
-	}
+	assert.Equal(t, IsNeighbor(b1, b2, 33), false)
 }
