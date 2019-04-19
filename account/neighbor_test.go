a205744d3f946050592f343754dfb42f09f6fd97
sunchunfeng <516108736@qq.com>
2019-04-19 17:45:33 +0800

    modify test

--- account/neighbor_test.go
+++ account/neighbor_test.go
@@ -20,5 +20,5 @@ func TestIsNeighbor(t *testing.T) {
 
 	b1 = NewBranch(1<<16 | 2 | 0)
 	b2 = NewBranch(3<<16 | 2 | 1)
-	assert.True(t, IsNeighbor(b1, b2, 33))
+	assert.False(t, IsNeighbor(b1, b2, 33))
 }
