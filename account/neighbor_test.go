28d46a29b608bd9514db0434ca5730ad5713e00b
sunchunfeng <516108736@qq.com>
2019-04-19 17:41:28 +0800

    use assert.True->assert.Equal

--- account/neighbor_test.go
+++ account/neighbor_test.go
@@ -8,18 +8,17 @@ import (
 func TestIsNeighbor(t *testing.T) {
 	b1 := NewBranch(2<<16 | 2 | 1)
 	b2 := NewBranch(2<<16 | 2 | 0)
-	assert.Equal(t, IsNeighbor(b1, b2, 33), true)
+	assert.True(t, IsNeighbor(b1, b2, 33))
 
 	b1 = NewBranch(1<<16 | 2 | 1)
 	b2 = NewBranch(3<<16 | 2 | 1)
-
-	assert.Equal(t, IsNeighbor(b1, b2, 33), true)
-
-	b1 = NewBranch(1<<16 | 2 | 0)
-	b2 = NewBranch(3<<16 | 2 | 1)
-	assert.Equal(t, IsNeighbor(b1, b2, 32), true)
+	assert.True(t, IsNeighbor(b1, b2, 33))
 
 	b1 = NewBranch(1<<16 | 2 | 0)
 	b2 = NewBranch(3<<16 | 2 | 1)
-	assert.Equal(t, IsNeighbor(b1, b2, 33), false)
+	assert.True(t, IsNeighbor(b1, b2, 32))
+
+	b1 = NewBranch(1<<16 | 2 | 0)
+	b2 = NewBranch(3<<16 | 2 | 1)
+	assert.True(t, IsNeighbor(b1, b2, 33))
 }
