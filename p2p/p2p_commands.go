d14c9af406846c06ec91d618957688f0700aec81
pingke <pingke@quarkchain.org>
2019-05-07 16:59:21 +0800

    add rpc call and sync task to p2p handler  (#109)
    * add peer & handler
    * add handle minor related p2p cmd and sync task related actions
    * add sync task and test case

--- p2p/p2p_commands.go
+++ p2p/p2p_commands.go
@@ -2,7 +2,6 @@ package p2p
 
 import (
 	"bytes"
-	"github.com/QuarkChain/goquarkchain/account"
 	"github.com/QuarkChain/goquarkchain/core/types"
 	"github.com/QuarkChain/goquarkchain/serialize"
 	"github.com/ethereum/go-ethereum/common"
@@ -202,15 +201,14 @@ type GetMinorBlockListResponse struct {
 //GetMinorBlockHeaderListRequest get minor block header list request
 type GetMinorBlockHeaderListRequest struct {
 	BlockHash common.Hash
-	Branch    account.Branch
 	Limit     uint32
 	Direction uint8
 }
 
 //GetMinorBlockHeaderListResponse get minor block header list response
 type GetMinorBlockHeaderListResponse struct {
-	RootTip         types.RootBlockHeader
-	ShardTip        types.MinorBlockHeader
+	RootTip         *types.RootBlockHeader
+	ShardTip        *types.MinorBlockHeader
 	BlockHeaderList []*types.MinorBlockHeader `bytesizeofslicelen:"4"`
 }
 
