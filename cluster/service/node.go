32a9de96efac18ddcf8389c0df3b28ead595837e
mask-pp <huanliu@quarkchain.org>
2019-05-12 02:49:11 -0700

    rm useless OpenDatabase func

--- cluster/service/node.go
+++ cluster/service/node.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	qkcrpc "github.com/QuarkChain/goquarkchain/cluster/rpc"
 	"github.com/QuarkChain/goquarkchain/p2p"
-	"github.com/QuarkChain/goquarkchain/qkcdb"
-	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -566,20 +564,6 @@ func (n *Node) HTTPEndpoint() string {
 // the current protocol stack.
 func (n *Node) EventMux() *event.TypeMux {
 	return n.eventmux
-}
-
-// OpenDatabase opens an existing database with the given name (or creates one if no
-// previous can be found) from within the node's instance directory. If the node is
-// ephemeral, a memory database is returned.
-func (n *Node) OpenDatabase(name string) (ethdb.Database, error) {
-	if n.config.DataDir == "" {
-		return ethdb.NewMemDatabase(), nil
-	}
-	path := n.config.ResolvePath(name)
-	if err := os.MkdirAll(path, 0700); err != nil {
-		return nil, err
-	}
-	return qkcdb.NewRDBDatabase(path)
 }
 
 // ResolvePath returns the absolute path of a resource in the instance directory.
