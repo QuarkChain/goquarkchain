b393c9b027a014cc0b0d38f9811e1bd2e88247c8
pingke <pingke@quarkchain.org>
2019-06-18 22:37:27 +0800

    add gm sm3 hash

---
+++ consensus/gmhash/gmhash.go
@@ -0,0 +1,80 @@
+package doublesha256
+
+import (
+	"encoding/binary"
+	"errors"
+	"github.com/QuarkChain/goquarkchain/crypto"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/QuarkChain/goquarkchain/consensus"
+	"github.com/QuarkChain/goquarkchain/core/state"
+	"github.com/QuarkChain/goquarkchain/core/types"
+)
+
+var (
+	// two256 is a big integer representing 2^256
+	two256 = new(big.Int).Exp(big.NewInt(2), big.NewInt(256), big.NewInt(0))
+)
+
+// GmSm3Hash is a consensus engine implementing PoW with Gm Sm3 algo.
+// See the interface definition:
+// Implements consensus.Pow
+type GmSm3Hash struct {
+	*consensus.CommonEngine
+}
+
+// Prepare initializes the consensus fields of a block header according to the
+// rules of a particular engine. The changes are executed inline.
+func (d *GmSm3Hash) Prepare(chain consensus.ChainReader, header types.IHeader) error {
+	panic("not implemented")
+}
+
+func (d *GmSm3Hash) Finalize(chain consensus.ChainReader, header types.IHeader, state *state.StateDB, txs []*types.Transaction, receipts []*types.Receipt) (types.IBlock, error) {
+	panic(errors.New("not finalize"))
+}
+
+func hashAlgo(height uint64, hash []byte, nonce uint64) (consensus.MiningResult, error) {
+	nonceBytes := make([]byte, 8)
+	// Note it's big endian here
+	binary.BigEndian.PutUint64(nonceBytes, nonce)
+	hashNonceBytes := append(hash, nonceBytes...)
+
+	hashOnce := crypto.SM3Hash(hashNonceBytes)
+	resultArray := crypto.SM3Hash(hashOnce[:])
+	return consensus.MiningResult{
+		Digest: common.Hash{},
+		Result: resultArray[:],
+		Nonce:  nonce,
+	}, nil
+}
+
+func verifySeal(chain consensus.ChainReader, header types.IHeader, adjustedDiff *big.Int) error {
+	if header.GetDifficulty().Sign() <= 0 {
+		return consensus.ErrInvalidDifficulty
+	}
+	diff := adjustedDiff
+	if diff == nil || diff.Cmp(big.NewInt(0)) == 0 {
+		diff = header.GetDifficulty()
+	}
+
+	target := new(big.Int).Div(two256, diff)
+	miningRes, _ := hashAlgo(0 /* not used */, header.SealHash().Bytes(), header.GetNonce())
+	if new(big.Int).SetBytes(miningRes.Result).Cmp(target) > 0 {
+		return consensus.ErrInvalidPoW
+	}
+	return nil
+}
+
+// New returns a GmSm3Hash scheme.
+func New(diffCalculator consensus.DifficultyCalculator, remote bool) *GmSm3Hash {
+	spec := consensus.MiningSpec{
+		Name:       "GmSm3Hash",
+		HashAlgo:   hashAlgo,
+		VerifySeal: verifySeal,
+	}
+	return &GmSm3Hash{
+		CommonEngine: consensus.NewCommonEngine(spec, diffCalculator, remote),
+	}
+}
---
+++ consensus/gmhash/gmhash_test.go
@@ -0,0 +1,36 @@
+package doublesha256
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/QuarkChain/goquarkchain/consensus"
+	"github.com/QuarkChain/goquarkchain/core/types"
+)
+
+func TestVerifySeal(t *testing.T) {
+	assert := assert.New(t)
+	diffCalculator := consensus.EthDifficultyCalculator{AdjustmentCutoff: 7, AdjustmentFactor: 512, MinimumDifficulty: big.NewInt(100000)}
+
+	header := &types.RootBlockHeader{Number: 1, Difficulty: big.NewInt(10)}
+	rootBlock := types.NewRootBlockWithHeader(header)
+	d := New(&diffCalculator, false)
+
+	resultsCh := make(chan types.IBlock)
+	err := d.Seal(nil, rootBlock, resultsCh, nil)
+	assert.NoError(err, "should have no problem sealing the block")
+	block := <-resultsCh
+
+	// Correct
+	header.Nonce = block.IHeader().GetNonce()
+	header.MixDigest = block.IHeader().GetMixDigest()
+	err = d.VerifySeal(nil, header, big.NewInt(0))
+	assert.NoError(err, "should have correct nonce")
+
+	// Wrong
+	header.Nonce = 0
+	err = d.VerifySeal(nil, header, big.NewInt(0))
+	assert.Error(err, "should have error because of the wrong nonce")
+}
