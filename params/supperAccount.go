ef280890dc2e04998d6566c666e6ea8ae1ed5df4
sunchunfeng <516108736@qq.com>
2019-07-06 12:08:43 +0800

    delete

--- params/supperAccount.go
+++ params/supperAccount.go
@@ -3,21 +3,4 @@ package params
 var (
 	AccountEnabled  = []byte{1}
 	AccountDisabled = []byte{0}
-	//superAccountStrList = []string{"0x438befb16aed2d01bc0ba111eee12c65dcdb5275"}
-	//superAccount        = make(map[account.Recipient]struct{}, 0)
 )
-
-//func init() {
-//	for _, v := range superAccountStrList {
-//		addr := common.BytesToAddress(common.FromHex(v))
-//		superAccount[addr] = struct{}{}
-//	}
-//}
-//
-//func IsSuperAccount(addr account.Recipient) bool {
-//	_, ok := superAccount[addr]
-//	return ok
-//}
-//func SetSuperAccount(address account.Recipient) { //only in test
-//	superAccount[address] = struct{}{}
-//}
