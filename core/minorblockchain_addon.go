f656822f519ea472dfe1132db2cb4bc0c9173a68
chunfengSun <516108736@qq.com>
2019-07-11 15:05:08 +0800

    minor block 0 add double (#237)
    minor block 0 add double

--- core/minorblockchain_addon.go
+++ core/minorblockchain_addon.go
@@ -60,7 +60,10 @@ func (m *MinorBlockChain) putMinorBlock(mBlock *types.MinorBlock, xShardReceiveT
 		m.heightToMinorBlockHashes[mBlock.NumberU64()] = make(map[common.Hash]struct{})
 	}
 	m.heightToMinorBlockHashes[mBlock.NumberU64()][mBlock.Hash()] = struct{}{}
-	rawdb.WriteMinorBlock(m.db, mBlock)
+	if !m.HasBlock(mBlock.Hash()) {
+		rawdb.WriteMinorBlock(m.db, mBlock)
+	}
+
 	if err := m.putTotalTxCount(mBlock); err != nil {
 		return err
 	}
