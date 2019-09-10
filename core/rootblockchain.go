680ade4a207104a33db5ec0d4114d9422b98a94a
sunchunfeng <516108736@qq.com>
2019-09-10 16:05:00 +0800

    czczcxz

--- core/rootblockchain.go
+++ core/rootblockchain.go
@@ -1137,9 +1137,11 @@ func (m *RootBlockChain) GetAdjustedDifficulty(header types.IHeader) (*big.Int, 
 	fmt.Println("guardian_public_key", m.Config().GuardianPublicKey)
 	fmt.Println("sealHasa", rHeader.SealHash().String())
 	fmt.Println("Signate", hex.EncodeToString(rHeader.Signature[:64]))
-	if crypto.VerifySignature(common.FromHex(m.Config().GuardianPublicKey), rHeader.SealHash().Bytes(), rHeader.Signature[:64]) {
+	fmt.Println("status", crypto.VerifySignature(common.FromHex(m.Config().GuardianPublicKey), rHeader.SealHash().Bytes(), rHeader.Signature[:64]), header.GetDifficulty())
+	//if crypto.VerifySignature(common.FromHex(m.Config().GuardianPublicKey), rHeader.SealHash().Bytes(), rHeader.Signature[:64]) {
 		adjustedDiff = new(big.Int).Div(rHeader.GetDifficulty(), new(big.Int).SetUint64(1000))
-	}
+	//}
+	fmt.Println("adjustDiff", adjustedDiff)
 	return adjustedDiff, nil
 }
 
