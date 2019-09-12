f65c9e7d7c3ef3dbef7b446355847e680fbc2f5d
sunchunfeng <516108736@qq.com>
2019-09-12 10:50:40 +0800

    t

--- core/rootblockchain.go
+++ core/rootblockchain.go
@@ -3,7 +3,6 @@
 package core
 
 import (
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"github.com/QuarkChain/goquarkchain/internal/qkcapi"
@@ -1132,15 +1131,15 @@ func (m *RootBlockChain) SkipDifficultyCheck() bool {
 func (m *RootBlockChain) GetAdjustedDifficulty(header types.IHeader) (*big.Int, error) {
 	rHeader := header.(*types.RootBlockHeader)
 	adjustedDiff := rHeader.GetDifficulty()
-	fmt.Println("############")
-	fmt.Println("guardian_public_key", m.Config().GuardianPublicKey)
-	fmt.Println("sealHasa", rHeader.SealHash().String())
-	fmt.Println("Signate", hex.EncodeToString(rHeader.Signature[:64]))
-	fmt.Println("status", crypto.VerifySignature(common.FromHex(m.Config().GuardianPublicKey), rHeader.SealHash().Bytes(), rHeader.Signature[:64]), header.GetDifficulty())
+	//fmt.Println("############")
+	//fmt.Println("guardian_public_key", m.Config().GuardianPublicKey)
+	//fmt.Println("sealHasa", rHeader.SealHash().String())
+	//fmt.Println("Signate", hex.EncodeToString(rHeader.Signature[:64]))
+	//fmt.Println("status", crypto.VerifySignature(common.FromHex(m.Config().GuardianPublicKey), rHeader.SealHash().Bytes(), rHeader.Signature[:64]), header.GetDifficulty())
 	//if crypto.VerifySignature(common.FromHex(m.Config().GuardianPublicKey), rHeader.SealHash().Bytes(), rHeader.Signature[:64]) {
 	adjustedDiff = new(big.Int).Div(rHeader.GetDifficulty(), new(big.Int).SetUint64(1000))
 	//}
-	fmt.Println("adjustDiff", adjustedDiff)
+	//fmt.Println("adjustDiff", adjustedDiff)
 	return adjustedDiff, nil
 }
 
