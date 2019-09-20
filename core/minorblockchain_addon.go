d8c0398b67c51d0b4bf749e69fc49f5c8a5f11b0
sunchunfeng <516108736@qq.com>
2019-09-20 20:27:50 +0800

    ttttttttttt

--- core/minorblockchain_addon.go
+++ core/minorblockchain_addon.go
@@ -434,8 +434,7 @@ func (m *MinorBlockChain) runBlock(block *types.MinorBlock) (*state.StateDB, typ
 		return nil, nil, nil, 0, nil, ErrRootBlockIsNil
 	}
 	xShardReceiveTxList := make([]*types.CrossShardTransactionDeposit, 0)
-	coinbase := block.Coinbase().Recipient
-	preEvmState, err := m.stateAtWithSenderDisallowMap(parent, &coinbase)
+	preEvmState, err := m.getEvmStateForNewBlock(block.Header(), false)
 	fmt.Println("evmState.Coinbase",preEvmState.GetBlockCoinbase().String(),block.Header().GetCoinbase().FullShardKey)
 	if err != nil {
 		return nil, nil, nil, 0, nil, err
