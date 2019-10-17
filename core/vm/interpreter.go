7a5b16595352b3b41d3070d583fdd8eb89c05423
sunchunfeng <516108736@qq.com>
2019-10-17 15:16:47 +0800

    1516

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -212,6 +212,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		// Get the operation from the jump table and validate the stack to ensure there are
 		// enough stack items available to perform the operation.
 		op = contract.GetOp(pc)
+		fmt.Println("op", op.String(), contract.Gas)
 		operation := in.cfg.JumpTable[op]
 		if !operation.valid {
 			return nil, fmt.Errorf("invalid opcode 0x%x", int(op))
