0d0074ea9b3dbde6d17d3ac8cbd5e778a08564c1
pingke <pingke@quarkchain.org>
2019-10-26 18:25:13 +0800

    tx_generator.go optimize

--- cluster/shard/tx_generator.go
+++ cluster/shard/tx_generator.go
@@ -36,8 +36,8 @@ type AccountWithPrivateKey struct {
 	privateKey *ecdsa.PrivateKey
 }
 
-func getPrivateKeyFromAccount(data []*account.Account) []AccountWithPrivateKey {
-	accounts := make([]AccountWithPrivateKey, 0, len(data))
+func getAccountWithPrivateKey(data []*account.Account) []AccountWithPrivateKey {
+	accounts := make([]AccountWithPrivateKey, len(data))
 
 	for index, v := range data {
 		prvKey, err := crypto.HexToECDSA(v.PrivateKey())
@@ -53,14 +53,14 @@ func getPrivateKeyFromAccount(data []*account.Account) []AccountWithPrivateKey {
 }
 
 func NewTxGenerator(genesisDir string, fullShardId uint32, cfg *config.QuarkChainConfig) []*TxGenerator {
-	tgs := make([]*TxGenerator, 0, params.TPS_Num)
-	accounts := config.LoadtestAccounts(genesisDir)
+	tgs := make([]*TxGenerator, params.TPS_Num)
+	accounts := getAccountWithPrivateKey(config.LoadtestAccounts(genesisDir))
 	interval := len(accounts) / params.TPS_Num
 	for index := 0; index < params.TPS_Num; index++ {
 		tgs[index] = &TxGenerator{
 			cfg:          cfg,
 			fullShardId:  fullShardId,
-			accounts:     getPrivateKeyFromAccount(accounts[index*interval : (index+1)*interval]),
+			accounts:     accounts[index*interval : (index+1)*interval],
 			once:         sync.Once{},
 			lenAccounts:  interval,
 			accountIndex: 0,
@@ -91,7 +91,6 @@ func (t *TxGenerator) SignTxs(txs []*types.Transaction, span int) error {
 			}
 			return nil
 		})
-
 	}
 	if err := g.Wait(); err != nil {
 		return err
@@ -210,4 +209,97 @@ func (t *TxGenerator) random(digit int) int {
 
 func (t *TxGenerator) sign(evmTx *types.EvmTransaction, key *ecdsa.PrivateKey) (*types.EvmTransaction, error) {
 	return types.SignTx(evmTx, t.sender, key)
+}
+
+func (t *TxGenerator) Generate2(genTxs *rpc.GenTxRequest, addTxList func(txs []*types.Transaction, peerID string) error) error {
+	ts := time.Now()
+	tsa := time.Now()
+	var (
+		batchScale    = 4000
+		txList        = make([]*types.Transaction, batchScale)
+		numTx         = genTxs.NumTxPerShard
+		xShardPercent = int(genTxs.XShardPercent)
+		total         = uint32(0)
+		index         = 0
+	)
+	// return err if accounts is empty.
+	if t.accounts == nil {
+		return fmt.Errorf("accounts is empty, can't create transactions")
+	}
+	if numTx == 0 {
+		return fmt.Errorf("create txs operation, numTx is zero")
+	}
+	log.Info("Start Generating transactions", "tx count", numTx, "cross-shard tx count", xShardPercent)
+	for t.accountIndex < t.lenAccounts {
+		if total >= numTx {
+			break
+		}
+
+		tx, err := t.createTransaction2(t.accounts[t.accountIndex].privateKey, t.turn, xShardPercent, genTxs.Tx)
+		if err != nil {
+			continue
+		}
+		total++
+		txList[index] = &types.Transaction{TxType: types.EvmTx, EvmTx: tx}
+		index++
+
+		if index >= batchScale {
+			log.Info("detail", "total", total, "numTx", numTx, "durtion", time.Now().Sub(ts).Seconds(), "index", t.accountIndex)
+
+			if err := addTxList(txList, ""); err != nil {
+				return err
+			}
+			log.Info("addTxList end", "total", total, "numTx", numTx, "durtion", time.Now().Sub(ts).Seconds())
+			txList = txList[:0]
+			index = 0
+			ts = time.Now()
+		}
+
+		t.accountIndex++
+		if t.accountIndex == t.lenAccounts {
+			t.turn++
+			t.accountIndex = 0
+		}
+	}
+
+	if len(txList) != 0 {
+		if err := addTxList(txList, ""); err != nil {
+			return err
+		}
+	}
+
+	log.Info("Finish Generating transactions", "fullShardId", t.fullShardId, "tx count", total, "use seconds", time.Now().Sub(tsa))
+	return nil
+}
+
+func (t *TxGenerator) createTransaction2(prvKey *ecdsa.PrivateKey, nonce uint64,
+	xShardPercent int, sampleTx *types.Transaction) (*types.EvmTransaction, error) {
+	var (
+		fromFullShardKey = t.fullShardId
+		toFullShardKey   = fromFullShardKey
+		recipient        = common.Address{}
+	)
+	if sampleTx.EvmTx.To() != nil {
+		recipient = *sampleTx.EvmTx.To()
+	} else {
+		idx := t.random(t.lenAccounts)
+		toAddr := t.accounts[idx].acc
+		recipient = toAddr.Identity.GetRecipient()
+	}
+
+	if xShardPercent >= 0 && t.random(100) < xShardPercent {
+		fullShardIds := t.cfg.GetGenesisShardIds()
+		idx := uint32(t.random(len(fullShardIds)))
+		toFullShardKey = fullShardIds[idx]
+	}
+	value := sampleTx.EvmTx.Value()
+	if value.Uint64() == 0 {
+		rv := t.random(100)
+		value = value.Mul(big.NewInt(int64(rv)), config.QuarkashToJiaozi)
+	}
+
+	evmTx := types.NewEvmTransaction(nonce, recipient, value, params.DefaultTxGasLimit.Uint64(),
+		sampleTx.EvmTx.GasPrice(), fromFullShardKey, toFullShardKey, t.cfg.NetworkID, 0, sampleTx.EvmTx.Data(), qkcCommon.TokenIDEncode("QKC"), qkcCommon.TokenIDEncode("QKC"))
+
+	return types.SignTx(evmTx, t.sender, prvKey)
 }
