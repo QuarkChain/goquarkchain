48623ddf4dfd8e19d50274f380240a63ded47e8e
sunchunfeng <516108736@qq.com>
2019-10-31 18:35:39 +0800

    dasda

--- tests/loadtest/deployer/deploy_cluster.go
+++ tests/loadtest/deployer/deploy_cluster.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"time"
+
+	"github.com/ybbus/jsonrpc"
 
 	"github.com/QuarkChain/goquarkchain/tests/loadtest/deployer/deploy"
 	"github.com/ethereum/go-ethereum/log"
@@ -47,6 +51,26 @@ func main() {
 		toolManager.StartCluster(index)
 		log.Info("============end start cluster============", "index", index, "info", toolManager.LocalConfig.Hosts[index])
 		toolManager.ClusterIndex++
+	}
+
+	log.Info("ready to check status")
+	time.Sleep(10 * time.Second)
+	toolManager.ClusterIndex = 0
+	for index := 0; index < len(toolManager.LocalConfig.Hosts); index++ {
+		masterIP := toolManager.GetIpListDependTag("master")[0]
+		pubUrl := fmt.Sprintf("http://%s:38491", masterIP)
+		client := jsonrpc.NewClient(pubUrl)
+		resp, err := client.Call("getPeers")
+		if err != nil {
+			panic(fmt.Errorf("getPeer from ip %v err %v", masterIP, err))
+		}
+		if resp == nil {
+			panic(fmt.Errorf("getPeer from ip %v resp==nil", masterIP))
+		}
+		if resp.Error != nil {
+			panic(fmt.Errorf("getPeer from ip %v err %v", masterIP, resp.Error))
+		}
+		fmt.Println("masterIP", masterIP, "resp", resp.Result.(map[string]interface{}))
 	}
 
 }
