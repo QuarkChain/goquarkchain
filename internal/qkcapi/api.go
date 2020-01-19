1ff39faf671bef8ec3926b33e6e0739455d4260a
516108736 <516108736@qq.com>
2020-01-19 13:10:32 +0800

    1310

--- internal/qkcapi/api.go
+++ internal/qkcapi/api.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math/big"
 	"sort"
+	"time"
 
 	"github.com/QuarkChain/goquarkchain/account"
 	qrpc "github.com/QuarkChain/goquarkchain/cluster/rpc"
@@ -578,6 +579,7 @@ func (p *PublicBlockChainAPI) SubmitWork(fullShardKey *hexutil.Uint, headHash co
 }
 
 func (p *PublicBlockChainAPI) GetWork(fullShardKey *hexutil.Uint, coinbaseAddress *common.Address) ([]common.Hash, error) {
+	ts:=time.Now()
 	var fullShardId *uint32
 	if fullShardKey != nil {
 		id, err := getFullShardId(fullShardKey)
@@ -601,6 +603,7 @@ func (p *PublicBlockChainAPI) GetWork(fullShardKey *hexutil.Uint, coinbaseAddres
 	if work.OptionalDivider > 1 {
 		val = append(val, common.BytesToHash(qcom.Uint64ToBytes(work.OptionalDivider)))
 	}
+	fmt.Println("End to get work-rrrrrrrrrrr",fullShardId,coinbaseAddress.String(),time.Now().Sub(ts).Seconds())
 	return val, nil
 }
 
