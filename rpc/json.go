d5355c656e834fbc70ec1e5f4caa2f9e7c3b68a3
sunchunfeng <516108736@qq.ocm>
2020-02-17 20:43:03 +0800

    2042

--- rpc/json.go
+++ rpc/json.go
@@ -346,7 +346,7 @@ func parsePositionalArguments(rawArgs json.RawMessage, types []reflect.Type) ([]
 		}
 		argval := reflect.New(types[i])
 		if err := dec.Decode(argval.Interface()); err != nil {
-			if mp[i]{
+			if mp[i] && types[i].Kind() == reflect.Ptr{
 				continue
 			}
 			return nil, &invalidParamsError{fmt.Sprintf("invalid argument %d: %v", i, err)}
