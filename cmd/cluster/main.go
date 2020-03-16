bb4dbbe7d7b3b40c11622b692bd27fe68d95f1aa
516108736 <516108736@qq.com>
2020-03-16 10:51:12 +0800

    gc same as go-ethereum

--- cmd/cluster/main.go
+++ cmd/cluster/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	godebug "runtime/debug"
 	"sort"
@@ -98,10 +99,10 @@ func init() {
 			}
 		}
 		// Ensure Go's GC ignores the database cache for trigger percentage
-		//cache := ctx.GlobalInt(utils.CacheFlag.Name)
-		//gogc := math.Max(10, math.Min(90, 100*(float64(cache)/float64(allowance))))
-		//log.Debug("Sanitizing Go's GC trigger", "percent", int(gogc))
-		godebug.SetGCPercent(int(10))
+		cache := ctx.GlobalInt(utils.CacheFlag.Name)
+		gogc := math.Max(10, math.Min(90, 100*(float64(cache)/float64(allowance))))
+		log.Debug("Sanitizing Go's GC trigger", "percent", int(gogc))
+		godebug.SetGCPercent(int(gogc))
 
 		return nil
 	}
