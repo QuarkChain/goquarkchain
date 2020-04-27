de9549aad9b2f54e0b289eece0652d09482cee7d
516108736 <516108736@qq.com>
2020-04-27 15:28:53 +0800

    modify cluster sh

--- cmd/cluster/run_cluster.sh
+++ cmd/cluster/run_cluster.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-configPath=$1
+if [ x"$1" = x ]; then
+    configPath="../../mainnet/singularity/cluster_config_template.json"
+    echo "not set config. use default config . configPath="$configPath
+else
+    configPath=$1
+    echo "set config, configPath="$configPath
+fi
 
 slaveInfo=`grep -Po 'ID[" :]+\K[^"]+' $configPath | grep S`
 
@@ -8,13 +14,15 @@ slaveInfo=`grep -Po 'ID[" :]+\K[^"]+' $configPath | grep S`
 for value in $slaveInfo
 do
 	 cmd="./cluster --cluster_config "${configPath}" --service "${value}">> "${value}".log 2>&1 &"
-	 echo $cmd
 	 eval $cmd
+	 pid=$!
+	 echo "Start "${value}" successfull pid="$pid" logFile="${value}.log
 done
 
 sleep 5s
 
 # start master
 cmd="./cluster --cluster_config "${configPath}" >>master.log 2>&1 &"
-echo $cmd
 eval $cmd
+pid=$!
+echo "Start master successfull pid="$pid" logFile=master.log"
