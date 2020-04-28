0bcde809299ac6068bb52a4a93c250945637d377
516108736 <516108736@qq.com>
2020-04-28 11:19:18 +0800

    Revert "-c -l"
    This reverts commit 46f510911e77ce59b2457bd38284d6d7a740da95.

--- cmd/cluster/run_cluster.sh
+++ cmd/cluster/run_cluster.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
-configPath="../../mainnet/singularity/cluster_config_template.json"
-logPath="./"
-while getopts ":c:l:h:" opt
-do
-    case $opt in
-        c)
-        configPath=$OPTARG
-        ;;
-        l)
-        logPath=$OPTARG"/"
-        ;;
-        ?)
-        echo "unexecpted param $OPTARG"
-        exit 0
-    esac
-done
 
-echo "configPath=$configPath"
-echo "logPath=$logPath"
+if [ x"$1" = x ]; then
+    configPath="../../mainnet/singularity/cluster_config_template.json"
+    echo "not set config. use default config . configPath="$configPath
+else
+    configPath=$1
+    echo "set config, configPath="$configPath
+fi
 
 slaveInfo=`grep -Po 'ID[" :]+\K[^"]+' $configPath | grep S`
 
 # start slave
 for value in $slaveInfo
 do
-   sLog=$logPath${value}".log"
-	 cmd="./cluster --cluster_config "${configPath}" --service "${value}">> "$sLog" 2>&1 &"
+	 cmd="./cluster --cluster_config "${configPath}" --service "${value}">> "${value}".log 2>&1 &"
 	 eval $cmd
 	 pid=$!
-	 echo "Start "${value}"     successfull pid="$pid" logPath=$sLog"
+	 echo "Start "${value}" successfull pid="$pid" logFile="${value}.log
 done
 
 sleep 5s
 
 # start master
-mLog=$logPath"master.log"
-cmd="./cluster --cluster_config "${configPath}" >>$mLog 2>&1 &"
+cmd="./cluster --cluster_config "${configPath}" >>master.log 2>&1 &"
 eval $cmd
 pid=$!
-echo "Start master successfull pid="$pid" logPath=$mLog"
+echo "Start master successfull pid="$pid" logFile=master.log"
