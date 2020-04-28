f3085505eb9847ab7c2907984f89b7170b6f4ca0
chunfengSun <516108736@qq.com>
2020-04-28 12:29:44 +0800

    modify cluster sh (#594)
    * modify cluster sh
    
    * -c -l
    
    * Revert "-c -l"
    
    This reverts commit 46f510911e77ce59b2457bd38284d6d7a740da95.
    
    * add full logPath

--- cmd/cluster/run_cluster.sh
+++ cmd/cluster/run_cluster.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-configPath=$1
+if [ x"$1" = x ]; then
+    configPath="../../mainnet/singularity/cluster_config_template.json"
+    echo "not set config. use default config . configPath="$configPath
+else
+    configPath=$1
+    echo "set config, configPath="$configPath
+fi
 
 slaveInfo=`grep -Po 'ID[" :]+\K[^"]+' $configPath | grep S`
 
 # start slave
 for value in $slaveInfo
 do
-	 cmd="./cluster --cluster_config "${configPath}" --service "${value}">> "${value}".log 2>&1 &"
-	 echo $cmd
+   sLog=`pwd`/${value}.log
+	 cmd="./cluster --cluster_config "${configPath}" --service "${value}">> "$sLog" 2>&1 &"
 	 eval $cmd
+	 pid=$!
+	 echo "Start "${value}"     successfull pid="$pid" logFile=$sLog"
 done
 
 sleep 5s
 
 # start master
-cmd="./cluster --cluster_config "${configPath}" >>master.log 2>&1 &"
-echo $cmd
+mLog=`pwd`/master.log
+cmd="./cluster --cluster_config "${configPath}" >>$mLog 2>&1 &"
 eval $cmd
+pid=$!
+echo "Start master successfull pid="$pid" logFile=$mLog"
