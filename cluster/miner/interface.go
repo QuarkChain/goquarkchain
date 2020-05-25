061b75a8a711dab7d4dbf102a9670e0154d8715d
516108736 <516108736@qq.com>
2020-05-25 14:44:04 +0800

    fix miner

--- cluster/miner/interface.go
+++ cluster/miner/interface.go
@@ -1,9 +1,10 @@
 package miner
 
 import (
+	"math/big"
+
 	"github.com/QuarkChain/goquarkchain/account"
 	"github.com/QuarkChain/goquarkchain/core/types"
-	"math/big"
 )
 
 type MinerAPI interface {
@@ -11,5 +12,6 @@ type MinerAPI interface {
 	CreateBlockToMine(addr *account.Address) (types.IBlock, *big.Int, uint64, error)
 	InsertMinedBlock(types.IBlock) error
 	IsSyncing() bool
+	IsRemoteMining() bool
 	GetTip() uint64
 }
