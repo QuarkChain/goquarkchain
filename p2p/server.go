c66519bdc9240d955112ae2eaa12baf44db5faa6
516108736 <516108736@qq.com>
2020-06-17 10:18:16 +0800

    delete  unless code

--- p2p/server.go
+++ p2p/server.go
@@ -22,14 +22,12 @@ import (
 	"crypto/ecdsa"
 	"encoding/hex"
 	"errors"
-	"fmt"
+	"github.com/QuarkChain/goquarkchain/p2p/nodefilter"
 	"net"
 	"sort"
 	"sync"
 	"sync/atomic"
 	"time"
-
-	"github.com/QuarkChain/goquarkchain/p2p/nodefilter"
 
 	"github.com/QuarkChain/goquarkchain/p2p/discover"
 	"github.com/ethereum/go-ethereum/common"
@@ -680,7 +678,6 @@ running:
 		case <-ticker.C:
 			scheduleTasks()
 			periodicallyUnblacklist()
-			fmt.Println("CCCCCCCCC-1")
 
 		case n := <-srv.addstatic:
 			// This channel is used by AddPeer to add to the
