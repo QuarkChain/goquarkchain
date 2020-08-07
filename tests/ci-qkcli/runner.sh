230fd59d798575c7e66acf8bedfb5a4de46dce17
516108736 <516108736@qq.com>
2020-08-07 14:03:01 +0800

    small fix

--- tests/ci-qkcli/runner.sh
+++ tests/ci-qkcli/runner.sh
@@ -22,7 +22,7 @@ docker pull $IMAGE
 container=$(echo "qkc-${testcase#*-}" | cut -f 1 -d '.')
 
 docker run --name $container -d -p 38391 $IMAGE
-docker exec  $container -i  bash -c \
+docker exec  -i $container   bash -c \
   'cp ../../tests/ci-qkcli/start_go_devent.sh ./ && chmod +x ./start_go_devent.sh && ./start_go_devent.sh'
 port=$(docker port $container | awk -F':' '{print $2}')
 
