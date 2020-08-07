6ee53891a94a9cff46f6c34f247e7b195e28d4f2
516108736 <516108736@qq.com>
2020-08-07 11:59:10 +0800

    small fix

--- tests/ci-qkcli/start_go_devent.sh
+++ tests/ci-qkcli/start_go_devent.sh
@@ -2,7 +2,7 @@
 cd ../../consensus/qkchash/native && make && cd -
 go build && chmod +x cluster
 
-cp tests/ci-qkcli/cluster_config.json ./ && cd -
+cp ../../tests/ci-qkcli/cluster_config.json ./ && cd -
 mv ../../core/vm/contracts.go_back ../../core/vm/contracts.go
 
 chmod +x ./run_cluster.sh  && ./run_cluster.sh ./cluster_config.json
