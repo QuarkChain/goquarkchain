349444a47712bac3027bd9527d27fbf8758d5aab
pingke <pingke@quarkchain.org>
2021-02-14 10:39:54 +0800

    update chain_config

--- cal/chain_config.go
+++ cal/chain_config.go
@@ -10,8 +10,24 @@ import (
 )
 
 func main() {
-	str := "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S0/shard-262145/db"
-	db, err := qkcdb.NewDatabase(str, false, false)
+	paths := make([]string, 8)
+	paths[0] = "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S0/shard-1/db"
+	paths[1] = "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S1/shard-65537/db"
+	paths[2] = "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S2/shard-131073/db"
+	paths[3] = "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S3/shard-196609/db"
+	paths[4] = "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S0/shard-262145/db"
+	paths[5] = "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S1/shard-327681/db"
+	paths[6] = "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S2/shard-393217/db"
+	paths[7] = "/home/gocode/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/mainnet/S3/shard-458753/db"
+
+	for idx, path := range paths {
+		GetBalances(idx, path)
+	}
+}
+
+func GetBalances(idx int, path string) {
+	fmt.Println("--------", idx)
+	db, err := qkcdb.NewDatabase(path, false, false)
 	if err != nil {
 		panic(err)
 	}
@@ -21,7 +37,7 @@ func main() {
 	fmt.Println("seek", it.Key(), len(it.Key()), it.Valid())
 	distribute := make(map[int]int)
 	for it.Valid() {
-		fmt.Println("22")
+		//fmt.Println("22")
 		size := len(it.Key())
 		distribute[size]++
 		if size == 20 {
@@ -38,7 +54,7 @@ func main() {
 		}
 		it.Next()
 	}
-	fmt.Println("33")
+	fmt.Println("map len", len(distribute))
 	for key, value := range distribute {
 		fmt.Println(key, value)
 	}
