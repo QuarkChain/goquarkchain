110b938a8cd7948197695a1f641c5becb11984d3
516108736 <516108736@qq.com>
2021-07-05 17:00:41 +0800

    add metamask config

--- cmd/eth_api/main.go
+++ cmd/eth_api/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/QuarkChain/goquarkchain/common/hexutil"
 	"github.com/QuarkChain/goquarkchain/internal/qkcapi"
 	"github.com/QuarkChain/goquarkchain/rpc"
 	"github.com/ybbus/jsonrpc"
@@ -8,7 +12,7 @@ import (
 
 type ShardConfig struct {
 	ShardRPC    string
-	FullShardID uint32
+	FullShardID hexutil.Uint
 	EthChainID  uint32
 }
 
@@ -17,27 +21,21 @@ type MetaMaskConfig struct {
 	ShardConfigs []ShardConfig
 }
 
-func getMetaConfig() *MetaMaskConfig {
-	return &MetaMaskConfig{
-		QkcRPC: "http://67.207.94.27:38391",
-		//QkcRPC: "http://44.242.140.83:38391",
-		ShardConfigs: []ShardConfig{
-			{
-				ShardRPC:    "0.0.0.0:39001",
-				FullShardID: 0,
-				EthChainID:  110001,
-			},
-			{
-				ShardRPC:    "0.0.0.0:39002",
-				FullShardID: 65536,
-				EthChainID:  110002,
-			},
-		},
+func getMetaConfig(file string) *MetaMaskConfig {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		panic(err)
 	}
+	m := new(MetaMaskConfig)
+	err = json.Unmarshal(data, m)
+	if err != nil {
+		panic(err)
+	}
+	return m
 }
 
 func main() {
-	config := getMetaConfig()
+	config := getMetaConfig("./eth_api.json")
 	qkcClient := jsonrpc.NewClient(config.QkcRPC)
 
 	apis := make([][]rpc.API, 0)
@@ -46,7 +44,7 @@ func main() {
 		api = append(api, rpc.API{
 			Namespace: "eth",
 			Version:   "1.0",
-			Service:   qkcapi.NewShardAPI(shard.FullShardID, shard.EthChainID, qkcClient),
+			Service:   qkcapi.NewShardAPI(uint32(shard.FullShardID), shard.EthChainID, qkcClient),
 			Public:    true,
 		})
 		api = append(api, rpc.API{
