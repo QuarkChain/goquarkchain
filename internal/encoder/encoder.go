75333462535da056d6ad74328cb23159cde298c5
pingke <pingke@quarkchain.org>
2021-12-09 15:09:16 +0800

    remove unuse code

--- internal/encoder/encoder.go
+++ internal/encoder/encoder.go
@@ -248,8 +248,6 @@ func TxEncoder(block *types.MinorBlock, i int) (map[string]interface{}, error) {
 		return nil, err
 	}
 	field := map[string]interface{}{
-		"blockHash":        block.Hash(),
-		"blockNumber":      block.NumberU64(),
 		"id":               IDEncoder(tx.Hash().Bytes(), evmtx.FromFullShardKey()),
 		"hash":             tx.Hash(),
 		"nonce":            hexutil.Uint64(evmtx.Nonce()),
@@ -267,7 +265,6 @@ func TxEncoder(block *types.MinorBlock, i int) (map[string]interface{}, error) {
 		"value":            (*hexutil.Big)(evmtx.Value()),
 		"gasPrice":         (*hexutil.Big)(evmtx.GasPrice()),
 		"gas":              hexutil.Uint64(evmtx.Gas()),
-		"input":            hexutil.Bytes(evmtx.Data()),
 		"data":             hexutil.Bytes(evmtx.Data()),
 		"networkId":        hexutil.Uint64(evmtx.NetworkId()),
 		"transferTokenId":  hexutil.Uint64(evmtx.TransferTokenID()),
