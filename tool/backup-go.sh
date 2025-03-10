8abbf1c7e147ec6df01b04e314bfdafc1745cda5
ping-ke <pingke@quarkchain.org>
2025-03-10 16:58:20 +0800

    Update checkdb actions: download quarkchain snapshot from storage box instead of S3 (#666)
    * update checkDB
    
    * resolve comments

---
+++ tool/backup-go.sh
@@ -0,0 +1,22 @@
+sshpass -p $PriKey scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null u446960@u446960.your-storagebox.de:public-data/VERSION-GO VERSION-GO
+
+filename=`cat VERSION-GO`
+
+if [ $filename = "Archive-Go-A.tar.gz" ]; then
+  filename="Archive-Go-B.tar.gz"
+else
+  filename="Archive-Go-A.tar.gz"
+fi
+echo $filename
+
+
+rm $filename
+curl https://qkcmainnet-go.s3.amazonaws.com/data/`curl https://qkcmainnet-go.s3.amazonaws.com/data/LATEST`.tar.gz --output $filename
+
+sshpass -p $PriKey scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $filename u446960@u446960.your-storagebox.de:public-data/$filename
+
+echo $filename > VERSION-GO
+
+sshpass -p $PriKey scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null VERSION-GO u446960@u446960.your-storagebox.de:public-data/VERSION-GO
+
+rm $filename
